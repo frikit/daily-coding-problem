68aa6738125911a0f18eaf1c7ea6b4e55c7b8510
frikit <osipovvictor1994@gmail.com>
2021-01-27 20:52:16 +0000

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+val junit_version: String by project
+
+plugins {
+    java
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junit_version)
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = junit_version)
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
---
+++ src/test/kotlin/com/github/interview/MainKtTest.kt
@@ -0,0 +1,11 @@
+package com.github.interview
+
+import org.junit.jupiter.api.Test
+
+internal class MainKtTest {
+
+    @Test
+    fun testHelloWorldReturnsPersonalizedMessage() {
+        println("1")
+    }
+}
